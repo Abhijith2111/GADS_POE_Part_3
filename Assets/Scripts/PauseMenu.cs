b2866690f9bb6cc88166b9814866fcb351d183ae
LazyTallIndian <77723138+Abhijith2111@users.noreply.github.com>
2025-10-27 00:45:41 +0200

    Added functional Pause menu in game

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu;
+    private bool isPaused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!isPaused)
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+        isPaused = true;
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+        isPaused = false;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1;
+        isPaused = false;
+    }
+
+    public void Quit()
+    {
+        SceneManager.LoadScene(0);
+        Time.timeScale = 1;
+        isPaused = false;
+    }
+}
